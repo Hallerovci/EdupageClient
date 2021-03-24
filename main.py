a45c4eeb4ec6d33e7096338c9a59d6b416eee2a3
vlckoadam <vlckoadam3@gmail.com>
2021-03-24 19:19:35 +0100

    Attach license notice

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+#     EdupageClient - Open-source desktop Edupage Client for students
+#     Copyright (C) MMXXI Tomáš Lovrant & Adam Vlčko
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from PyQt5.QtWidgets import QGridLayout, QSizePolicy, QLineEdit
 from edupage_api import Edupage, BadCredentialsException
 from PyQt5 import QtWidgets, QtCore, QtGui
